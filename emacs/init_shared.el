a908d1ec46ca08ae94e9e70a79b6f5c688f7e50e
Patrick <patbecich@gmail.com>
2016-12-26 14:20:32 -0800

    move custom set variables

--- emacs/init_shared.el
+++ emacs/init_shared.el
@@ -61,22 +61,6 @@
 
 
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(custom-enabled-themes (quote (tango-dark)))
- '(haskell-process-type (quote stack-ghci))
- '(haskell-stylish-on-save t)
- '(markdown-command "/opt/local/bin/pandoc")
- '(package-selected-packages
-   (quote
-    (flycheck-haskell haskell-mode pdf-tools restclient flycheck-rust rust-mode rustfmt auto-package-update scala-mode paradox markdown-mode magit ensime elpy)))
- '(paradox-github-token t)
- '(tool-bar-mode nil))
 
 (setq ensime-startup-snapshot-notification nil)
 
