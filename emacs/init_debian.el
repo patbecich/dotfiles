3e731b89691e68528be053d8f5a20fbb3dd5c81f
Patrick <patbecich@gmail.com>
2016-12-26 16:05:40 -0800

    elpy for raspberry pi

--- emacs/init_debian.el
+++ emacs/init_debian.el
@@ -1,2 +1,7 @@
+(setenv "WORKON_HOME" "/home/patbecich/.virtualenv/")
+
+(setenv "PATH" (concat (getenv "PATH") ":/home/patbecich/.virtualenv/env/bin"))
+(setq exec-path (append exec-path '("/home/patbecich/.virtualenv/env/bin")))
+
 
 (load "~/dotfiles/emacs/init_shared.el")
