132dd830839c57371756de573f2209f376c724c8
Patrick Becich <patbecich@gmail.com>
2018-12-23 23:01:36 -0800

    ubuntu

---
+++ emacs/init_linux.el
@@ -0,0 +1,4 @@
+(setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
+(setq exec-path (append exec-path '("/usr/local/bin")))
+
+(setq exec-path (append exec-path '("/home/patrick/.local/bin/")))
